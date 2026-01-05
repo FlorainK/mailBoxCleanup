0f77c2495c41c5c032a2357264f0c71c25851a2d
Florian Kenner <wi21119@lehre.dhbw-stuttgart.de>
2026-01-05 18:48:29 +0100

    finish mail unpacking

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/emersion/go-imap/v2"
@@ -18,6 +19,14 @@ import (
 type EmailStreamDTO struct {
 	Reader *mail.Reader
 	SeqNum uint32
+	Raw    []byte
+}
+
+type UnpackedMailDTO struct {
+	DateSent time.Time
+	Sender   string
+	Body     string
+	Raw      []byte
 }
 
 func fetchInboxEmails(
@@ -84,19 +93,38 @@ func fetchInboxEmails(
 		}
 
 		out <- EmailStreamDTO{
+			Raw:    raw,
 			Reader: mr,
 			SeqNum: msg.SeqNum,
 		}
 	}
 }
 
-func unpackInboxEmail(in <-chan EmailStreamDTO) {
+func unpackEmailContent(in <-chan EmailStreamDTO, out chan<- UnpackedMailDTO) {
 	go func() {
 		for msg := range in {
 			h := msg.Reader.Header
 
+			dateSent, err := h.Date()
+			if err != nil {
+				dateSent = time.Time{}
+			}
+
+			sender := ""
+			fromList, err := h.AddressList("From")
+			if err == nil && len(fromList) > 0 {
+				if fromList[0].Address != "" {
+					sender = fromList[0].Address
+				} else {
+					sender = fromList[0].String()
+				}
+			}
+
 			subject, _ := h.Text("Subject")
 			log.Printf("Seq %d | Subject: %s", msg.SeqNum, subject)
+
+			var plain strings.Builder
+			var html strings.Builder
 
 			for {
 				part, err := msg.Reader.NextPart()
@@ -109,13 +137,37 @@ func unpackInboxEmail(in <-chan EmailStreamDTO) {
 
 				switch h := part.Header.(type) {
 				case *mail.InlineHeader:
-					body, _ := io.ReadAll(part.Body)
-					log.Printf("Inline text (%d bytes)", len(body))
+					mediaType, _, _ := h.ContentType()
 
-				case *mail.AttachmentHeader:
-					filename, _ := h.Filename()
-					log.Printf("Attachment: %s", filename)
+					b, err := io.ReadAll(part.Body)
+					if err != nil {
+						log.Printf("failed to read inline body: %v", err)
+						continue
+					}
+
+					switch strings.ToLower(mediaType) {
+					case "text/html":
+						html.Write(b)
+					case "text/plain":
+						plain.Write(b)
+					default:
+						plain.Write(b)
+					}
 				}
+			}
+			fullText := plain.String()
+			fullHTML := html.String()
+
+			bestBody := fullText
+			if strings.TrimSpace(bestBody) == "" {
+				bestBody = fullHTML
+			}
+
+			out <- UnpackedMailDTO{
+				DateSent: dateSent,
+				Sender:   sender,
+				Body:     bestBody,
+				Raw:      msg.Raw,
 			}
 		}
 	}()
@@ -141,10 +193,17 @@ func main() {
 
 	log.Println("Logged in")
 
-	ch := make(chan EmailStreamDTO, 10)
+	emailStreamChannel := make(chan EmailStreamDTO, 50)
+	unpackedEmailStreamChannel := make(chan UnpackedMailDTO, 50)
 
-	go fetchInboxEmails(c, ch)
-	go unpackInboxEmail(ch)
+	go fetchInboxEmails(c, emailStreamChannel)
+	go unpackEmailContent(emailStreamChannel, unpackedEmailStreamChannel)
+
+	// next steps for the pipeline:
+	// classify whether to keep, delete
+	// save as file
+	// save id as checked
+	// delete the email
 
 	time.Sleep(time.Minute)
 }
